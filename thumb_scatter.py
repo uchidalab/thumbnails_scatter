f34d6cc8cb8e964435413141dca4af9b4f1253d7
Wataru Ohyama <ohyama@Primenova.local>
2018-08-01 15:21:31 +0900

    add "--emphasize" option

--- thumb_scatter.py
+++ thumb_scatter.py
@@ -6,10 +6,11 @@ ___doc___ ="""{f}
 thumb_scatter.py - making a scatter plot with thumbnail images
 
 Usage:
-	{f}	[-h|--help] [--scale=<scale>] <input_csv_file> <plot_title> <output_file>
+	{f}	[-h|--help] [--scale=<scale>] [--emphasize=<class>] <input_csv_file> <plot_title> <output_file>
 
 Options:
 	--scale : size of thumnail (real number : default = 1.0)
+	--emphasize : Class to be emphasize (4 digit hexadecimal code)
 	-h or --help : display this help message
 
 """.format(f=__file__)
@@ -35,6 +36,7 @@ InputCSV=''
 PlotTitle=''
 OutFile=''
 Scale = 1.0
+EmpCode = ''
 
 Num = 60
 
@@ -44,6 +46,7 @@ def parse_options () :
 	global PlotTitle
 	global OutFile
 	global Scale
+	global EmpCode
 
 	args = docopt ( ___doc___ )
 
@@ -54,7 +57,29 @@ def parse_options () :
 	if args['--scale']:
 		Scale = float ( args['--scale'] )
 		print ( 'scale = {}'.format(Scale))
+	if args['--emphasize']:
+		EmpCode = args['--emphasize']
+		print ( 'scale = {}'.format(Scale))
 
+
+def frame_image(img, frame_width):
+	b = frame_width # border size in pixel
+	ny, nx = img.shape[0], img.shape[1] # resolution / number of pixels in x and y
+	if img.ndim == 3: # rgb or rgba array
+		framed_img = np.zeros((b+ny+b, b+nx+b, img.shape[2]))
+	elif img.ndim == 2: # grayscale image
+		framed_img = np.zeros((b+ny+b, b+nx+b))
+	framed_img[b:-b, b:-b] = img
+	return framed_img
+
+
+def get_class ( fname ):
+	import os
+	basename = os.path.basename(fname)
+	ret = basename.split('_')[0]
+	return ret
+
+		
 def scatter_image(feature_x, feature_y, image_paths, title, save=None):
 	"""
 	Args:
@@ -71,10 +96,16 @@ def scatter_image(feature_x, feature_y, image_paths, title, save=None):
 
 	for (x, y, path) in zip(feature_x, feature_y, image_paths):
 		img = plt.imread(path)
+
 		disp_size = max ( xlim[1]-xlim[0], ylim[1]-ylim[0] ) / Num
 		bb = Bbox.from_bounds(x, y, disp_size*Scale, disp_size * Scale)
 		bb2 = TransformedBbox(bb, ax.transData)
 		bbox_image = BboxImage(bb2, norm=None, origin=None, clip_on=False)
+
+		if EmpCode != "" and get_class ( path ) == EmpCode :
+			print ( "{}".format (EmpCode) )
+			img = frame_image ( img, 15 )
+
 		bbox_image.set_data(img)
 		ax.add_artist(bbox_image)
 	ax.set_ylim(*ylim)
