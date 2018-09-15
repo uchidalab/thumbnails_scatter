4bf84f4319e3a5e8a6a9b8446ba1c2bf58c2d982
Wataru Ohyama <ohyama@ait.kyushu-u.ac.jp>
2018-09-15 15:35:47 +0900

    Add colorization of each character by the class

--- thumb_scatter.py
+++ thumb_scatter.py
@@ -6,11 +6,12 @@ ___doc___ ="""{f}
 thumb_scatter.py - making a scatter plot with thumbnail images
 
 Usage:
-	{f}	[-h|--help] [--scale=<scale>] [--emphasize=<class>] <input_csv_file> <plot_title> <output_file>
+	{f}	[-h|--help] [--scale=<scale>] [--emphasize=<class>] [--color] <input_csv_file> <plot_title> <output_file>
 
 Options:
 	--scale : size of thumnail (real number : default = 1.0)
 	--emphasize : Class to be emphasize (4 digit hexadecimal code)
+	--color : colorize character frames according to their colasses
 	-h or --help : display this help message
 
 """.format(f=__file__)
@@ -22,6 +23,7 @@ Options:
 from docopt import docopt
 import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 from matplotlib.image import BboxImage
 from matplotlib.transforms import Bbox, TransformedBbox
 import numpy as np
@@ -37,7 +39,7 @@ PlotTitle=''
 OutFile=''
 Scale = 1.0
 EmpCode = ''
-
+UseColor = False
 Num = 60
 
 def parse_options () :
@@ -47,6 +49,7 @@ def parse_options () :
 	global OutFile
 	global Scale
 	global EmpCode
+	global UseColor
 
 	args = docopt ( ___doc___ )
 
@@ -59,16 +62,22 @@ def parse_options () :
 		print ( 'scale = {}'.format(Scale))
 	if args['--emphasize']:
 		EmpCode = args['--emphasize']
-		print ( 'scale = {}'.format(Scale))
+		print ( 'pmphasize = {}'.format(EmpCode))
+	if args['--color']:
+		UseColor = True
+		print ( 'color')
 
-
-def frame_image(img, frame_width):
+def frame_image(img, frame_width, value, cmap = None):
 	b = frame_width # border size in pixel
 	ny, nx = img.shape[0], img.shape[1] # resolution / number of pixels in x and y
 	if img.ndim == 3: # rgb or rgba array
 		framed_img = np.zeros((b+ny+b, b+nx+b, img.shape[2]))
 	elif img.ndim == 2: # grayscale image
 		framed_img = np.zeros((b+ny+b, b+nx+b))
+
+	if cmap != None :
+		framed_img[:,:] = np.asarray(cmap.jet(value))[0:3]
+
 	framed_img[b:-b, b:-b] = img
 	return framed_img
 
@@ -80,7 +89,7 @@ def get_class ( fname ):
 	return ret
 
 		
-def scatter_image(feature_x, feature_y, image_paths, title, save=None):
+def scatter_image(feature_x, feature_y, image_paths, title, save=None, code_list=None):
 	"""
 	Args:
 	feature_x: x座標
@@ -100,12 +109,15 @@ def scatter_image(feature_x, feature_y, image_paths, title, save=None):
 		disp_size = max ( xlim[1]-xlim[0], ylim[1]-ylim[0] ) / Num
 		bb = Bbox.from_bounds(x, y, disp_size*Scale, disp_size * Scale)
 		bb2 = TransformedBbox(bb, ax.transData)
-		bbox_image = BboxImage(bb2, cmap='gray', norm=None, origin=None, clip_on=False)
+		bbox_image = BboxImage(bb2, cmap=None, norm=None, origin=None, clip_on=False)
 
 		if EmpCode != "" and get_class ( path ) == EmpCode :
-			print ( "{}".format (EmpCode) )
-			img = frame_image ( img, 15 )
-
+			img = frame_image ( img, 15, 0 )
+			
+		elif code_list != None :
+			idx = code_list.index ( get_class (path) )
+			img = frame_image ( img, 30, float(idx) / len(code_list), cmap=cm )
+			
 		bbox_image.set_data(img)
 		ax.add_artist(bbox_image)
 	ax.set_ylim(*ylim)
@@ -123,15 +135,22 @@ if __name__ == '__main__':
 	X = []
 	Y = []
 	images = []
-	
+	code_list = list()
 	with open (InputCSV, 'r') as fin :
 		reader = csv.reader(fin)
 		for row in reader :
 			X.append ( row[0] )
 			Y.append ( row[1] )
 			images.append ( row[2] )
+			code = get_class ( row[2] )
+			if (code in code_list) == False : 
+				code_list.append(code)
 
 	featX = np.array(X, dtype=float)
 	featY = np.array(Y, dtype=float)
 
-	scatter_image ( featX, featY, images, PlotTitle, save = OutFile )
+	if UseColor == True:
+		code_list.sort()
+		scatter_image ( featX, featY, images, PlotTitle, code_list = code_list, save = OutFile )
+	else:
+		scatter_image ( featX, featY, images, PlotTitle, save = OutFile )
